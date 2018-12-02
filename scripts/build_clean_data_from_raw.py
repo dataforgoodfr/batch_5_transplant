b0c921e55b1d63207667f9419cd796379a3d7b87
Max-Pol <maxpol.lebrun@gmail.com>
2018-12-03 00:56:11 +0100

    [data][scripts] Add date from static to dynamic.csv

--- scripts/build_clean_data_from_raw.py
+++ scripts/build_clean_data_from_raw.py
@@ -1,7 +1,10 @@
 import os
 import glob
+from datetime import datetime, timedelta
+import numpy as np
 import pandas as pd
-from transplant.config import PATH_DYNAMIC_RAW, PATH_DYNAMIC_CLEAN, PATH_STATIC_RAW, PATH_STATIC_CLEAN
+from transplant.config import PATH_DYNAMIC_RAW, PATH_DYNAMIC_CLEAN,\
+    PATH_STATIC_RAW, PATH_STATIC_CLEAN
 
 
 # Dynamic variables methods
@@ -17,7 +20,7 @@ def load_dynamic_raw(path_dynamic_raw):
     return pd.concat(dfs, sort=True)
 
 
-def clean_dynamic_raw(df):
+def clean_dynamic_raw(df, df_static):
     '''Clean dynamic raw DataFrame'''
     # Rename some columns
     df = df.rename(columns={'Unnamed: 0': 'id_patient', 'Unnamed: 1': 'time'})
@@ -34,7 +37,23 @@ def clean_dynamic_raw(df):
     # Format dtypes
     df['id_patient'] = df['id_patient'].astype(int)
 
+    # Convert time column to a real datetime.
+    # We need to join df_static to retrieve the date and apply
+    # a correction if the timestamps pass through midnight
+    df = df.merge(df_static[['id_patient', 'date_transplantation']])
+    df['time'] = pd.to_datetime(
+        df['date_transplantation'].dt.strftime('%Y-%m-%d') + ' ' + df['time'])
+    df = df.groupby('id_patient').apply(correct_date_shift)
+    df = df.drop(columns=['date_transplantation'])
+
     return df
+
+
+def correct_date_shift(x):
+    idx_0h = (x['time'] - x['time'].shift(1)) < timedelta(days=0)  # 0h indexes
+    for i in idx_0h.index[idx_0h]:  # loop over all 0h index
+        x['time'].loc[i:] = x['time'].loc[i:] + timedelta(days=1)
+    return x
 
 
 # Static variables methods
@@ -58,6 +77,10 @@ def clean_static_raw(df):
     # Drop useless data
     df = df.dropna(subset=['id_patient'])  # drop rows with nan in id_patient
 
+    # Replace wrong dates in date_sortie_bloc by NaNs
+    df_static.loc[df_static['date_sortie_bloc'] < datetime(2000, 1, 1),
+                  'date_sortie_bloc'] = np.NaN
+
     # Format dtypes
     df['id_patient'] = df['id_patient'].astype(int)
 
@@ -68,13 +91,13 @@ def clean_static_raw(df):
 #######################################
 if __name__ == '__main__':
     print('Building clean csv for static data...')
-    df = load_static_raw(PATH_STATIC_RAW)
-    df = clean_static_raw(df)
-    df.to_csv(PATH_STATIC_CLEAN, index=False)
+    df_static = load_static_raw(PATH_STATIC_RAW)
+    df_static = clean_static_raw(df_static)
+    df_static.to_csv(PATH_STATIC_CLEAN, index=False)
 
     print('Building clean csv for dynamic data...')
-    df = load_dynamic_raw(PATH_DYNAMIC_RAW)
-    df = clean_dynamic_raw(df)
-    df.to_csv(PATH_DYNAMIC_CLEAN, index=False)
+    df_dynamic = load_dynamic_raw(PATH_DYNAMIC_RAW)
+    df_dynamic = clean_dynamic_raw(df_dynamic, df_static)
+    df_dynamic.to_csv(PATH_DYNAMIC_CLEAN, index=False)
 
     print('GREAT SUCCESS!')
