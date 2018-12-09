0cc31a59e968fb3151db49a77e8e6fd1e48849ee
Pierre Delanoue <delanoue.pierre@gmail.com>
2018-12-09 11:39:00 +0100

    Nouvelle branche avec SLR qui ne marche pas du tout ...

--- transplant/tools/dataset.py
+++ transplant/tools/dataset.py
@@ -77,7 +77,7 @@ class Dataset:
                                              test_size=0.3,
                                              random_state=self._random_state)
 
-        test_df = self._drop_target_column(test_df)
+        #test_df = self._drop_target_column(test_df)
 
         return train_df, test_df
 
